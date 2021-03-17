d315187e9a4b3db09c2b22c1e96993db6d4221f5
Jeremie Melly <melly.jeremie@gmail.com>
2021-03-17 18:13:53 +0100

    Initial commit

---
+++ deuxentiers/src/test/java/ch/heigvd/deuxentiers/DeuxEntiersTest.java
@@ -0,0 +1,12 @@
+package ch.heigvd.deuxentiers;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Test;
+
+public class DeuxEntiersTest {
+    @Test
+    void divise() {
+        DeuxEntiers septTrois = new DeuxEntiers(7, 3);
+        assertEquals(2, septTrois.divise());
+    }
+}
